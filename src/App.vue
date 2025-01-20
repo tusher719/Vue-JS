bc5bd577c07997fd289d7103059c9b41c5eb2fe1
Tusher <62649759+tusher719@users.noreply.github.com>
2025-01-21 00:14:05 +0600

    tailwind v-2.0

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Navbar from './components/Navbar.vue'
+import Hero from './components/Hero.vue'
+import HomeCard from './components/HomeCard.vue'
+import JobListings from './components/JobListings.vue'
+</script>
+
+<template>
+  <Navbar />
+  <Hero />
+  <HomeCard />
+  <JobListings />
+</template>
