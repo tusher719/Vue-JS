386cf24a2713b0d0fcfecf6af4250b9df04915db
Tusher <62649759+tusher719@users.noreply.github.com>
2025-01-21 14:36:04 +0600

    v-3.1 (Vue Router & Home Vue)

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,7 @@
 <script setup>
-import Navbar from './components/Navbar.vue'
-import Hero from './components/Hero.vue'
-import HomeCard from './components/HomeCard.vue'
-import JobListings from './components/JobListings.vue'
+import Navbar from './components/Navbar.vue';
 </script>
 
 <template>
   <Navbar />
-  <Hero />
-  <HomeCard />
-  <JobListings :limit="3" :showButton="true" />
 </template>
