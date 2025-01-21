279c3fed12de6f572e980f0c662095e26ba89bb7
Tusher <62649759+tusher719@users.noreply.github.com>
2025-01-21 14:41:50 +0600

    v-3.2 (Vue Router & Home Vue)

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
-import './assets/main.css'
-import 'primeicons/primeicons.css'
-import router from './router'
+import './assets/main.css';
+import 'primeicons/primeicons.css';
+import router from './router';
 
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
 const app = createApp(App)
-app.use(router)
-app.mount('#app')
+
+app.use(router);
+
+app.mount('#app');
