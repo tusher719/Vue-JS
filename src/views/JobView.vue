a33476bc5618cd8468c18bc058c759fd93e65fa4
Tusher <62649759+tusher719@users.noreply.github.com>
2025-01-24 11:59:25 +0600

    v-3.4.1 (Proxying)

--- src/views/JobView.vue
+++ src/views/JobView.vue
@@ -16,7 +16,7 @@ const state = reactive({
 
 onMounted(async () => {
     try {
-        const response = await axios.get(`http://localhost:8000/jobs/${ jobId }`);
+        const response = await axios.get(`/api/jobs/${ jobId }`);
         state.job = response.data;
     } catch (error) {
         console.error("Error fetching job", error);
--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,13 @@ export default defineConfig({
   ],
   server: {
     port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      }
+    }
   },
   resolve: {
     alias: {
