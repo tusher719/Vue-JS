d4ed20c834ec6903cb251d9576dd56b8a03f5572
Tusher <62649759+tusher719@users.noreply.github.com>
2025-01-24 11:55:01 +0600

    v-3.3.2 (BackButton Component)

--- src/views/JobView.vue
+++ src/views/JobView.vue
@@ -1,5 +1,6 @@
 <script setup>
 import PulseLoader from "vue-spinner/src/PulseLoader.vue";
+import BackButton from "@/components/BackButton.vue";
 import { reactive, onMounted } from "vue";
 import { useRoute, RouterLink } from "vue-router";
 import axios from "axios";
@@ -27,6 +28,7 @@ onMounted(async () => {
 </script>
 
 <template>
+    <BackButton />
     <section v-if="!state.isLoading" class="bg-green-50">
         <div class="container m-auto py-10 px-6">
             <div class="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
@@ -95,11 +97,11 @@ onMounted(async () => {
                     <!-- Manage -->
                     <div class="bg-white p-6 rounded-lg shadow-md mt-6">
                         <h3 class="text-xl font-bold mb-6">Manage Job</h3>
-                        <a
-                            href="add-job.html"
-                            class="bg-green-500 hover:bg-green-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
-                            >Edit Job</a
-                        >
+                        <RouterLink
+                            :to="`/jobs/edit/${ state.job.id }`"
+                            class="bg-green-500 hover:bg-green-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
+                            Edit Job
+                        </RouterLink>
                         <button
                             class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                         >
