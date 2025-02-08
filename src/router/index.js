4a1ed59f77db15bd0e210800072574d5b14fc189
Tusher <62649759+tusher719@users.noreply.github.com>
2025-02-08 20:02:33 +0600

    v-3.4.5 (Job Updated)

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import JobsView from "@/views/JobsView.vue";
 import NotFoundView from "@/views/NotFoundView.vue";
 import JobView from "@/views/JobView.vue";
 import AddJobView from "@/views/AddJobView.vue";
+import EditJobView from "@/views/EditJobView.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,6 +23,11 @@ const router = createRouter({
             path: '/jobs/:id',
             name: 'job',
             component: JobView,
+        },
+        {
+            path: '/jobs/edit/:id',
+            name: 'edit-job',
+            component: EditJobView,
         },
         {
             path: '/jobs/add',
